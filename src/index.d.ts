9ff1a6469f022afca88734ebd8e55ab0bf9bdc63
Akos Szabacsi <akos.szabacsi@gmail.com>
2018-04-29 08:23:06 +0200

    fixed attribute names

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,15 +3,15 @@ import * as React from 'react';
 declare const scrollTo = (anchor: string) => void {} 
 
 interface AnchoredProps {
-  anchored: string;
+  anchor: string;
   children: JSX.Element | React.Component | Array<JSX.Element> | string | number;
 };
 
-declare const Anchored = ({anchored, children}: AnchoredProps): JSX.Element => {};
+declare const Anchored = ({anchor, children}: AnchoredProps): JSX.Element => {};
 
 interface ScrollToProps {
-  anchored: string;
+  anchor: string;
   children: JSX.Element | React.Component | Array<JSX.Element> | string | number;
 };
 
-declare const ScrollTo = ({anchored, children}: ScrollToProps): JSX.Element => {};
+declare const ScrollTo = ({anchor, children}: ScrollToProps): JSX.Element => {};
