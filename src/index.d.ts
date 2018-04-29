095feb8361473f079e0b4793b2018641f65f5a07
Akos Szabacsi <akos.szabacsi@gmail.com>
2018-04-29 09:29:39 +0200

    index.d.ts fixed

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,17 +1,17 @@
 import * as React from 'react';
 
-declare const scrollTo = (anchor: string) => void {} 
+declare function scrollTo(anchor: string): void;
 
 interface AnchoredProps {
   anchor: string;
   children: JSX.Element | React.Component | Array<JSX.Element> | string | number;
-};
+}
 
-declare const Anchored = ({anchor, children}: AnchoredProps): JSX.Element => {};
+declare function Anchored({anchor, children}: AnchoredProps): JSX.Element;
 
 interface ScrollToProps {
   anchor: string;
   children: JSX.Element | React.Component | Array<JSX.Element> | string | number;
-};
+}
 
-declare const ScrollTo = ({anchor, children}: ScrollToProps): JSX.Element => {};
+declare function ScrollTo({anchor, children}: ScrollToProps): JSX.Element;
