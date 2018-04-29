3f25b1b006dfe4048cde7a3de06ea6c420c3c395
Akos Szabacsi <akos.szabacsi@gmail.com>
2018-04-29 08:06:25 +0200

    fixed declaration file

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,14 +4,14 @@ declare const scrollTo = (anchor: string) => void {}
 
 interface AnchoredProps {
   anchored: string;
-  children: JSX.Element | React.Component | Array<JSX.Element>;
+  children: JSX.Element | React.Component | Array<JSX.Element> | string | number;
 };
 
 declare const Anchored = ({anchored, children}: AnchoredProps): JSX.Element => {};
 
 interface ScrollToProps {
   anchored: string;
-  children: JSX.Element | React.Component | Array<JSX.Element>;
+  children: JSX.Element | React.Component | Array<JSX.Element> | string | number;
 };
 
 declare const ScrollTo = ({anchored, children}: ScrollToProps): JSX.Element => {};
